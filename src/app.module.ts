719eca04cfd3a8ad946b87464becc2cd99ce21f1
Volodymyr Kolisnichenko <more4ocean@gmail.com>
2025-05-13 13:57:31 +0200

    create: docker-compose for Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
-import { OrdersModule } from './orders/orders.module';
 import { CategoryModule } from './category/category.module';
 import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [ProductModule,  CategoryModule, OrderModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      retryAttempts: 5,
+      retryDelay: 3000,
+    }),
+    ProductModule,
+    CategoryModule,
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
