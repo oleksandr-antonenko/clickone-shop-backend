88f133806e6a9bcb1d758ddd4f4faa01b91cb771
yevhenii-sulim <bees127@ukr.name>
2025-05-13 22:23:02 +0300

    added fastify

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ if (!process.env.IS_TS_NODE) {
   require('module-alias/register');
 }
 import { NestFactory } from '@nestjs/core';
+import {
+  NestFastifyApplication,
+  FastifyAdapter,
+} from '@nestjs/platform-fastify';
 import { AppModule } from '~/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   await app.listen(process.env.PORT ?? 3310);
 }
 void bootstrap();
