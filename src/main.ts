9adb3b5720338626cfc4a9fa4b758f1791937732
yevhenns <gifestum@gmail.com>
2025-05-14 16:01:00 +0300

    added swagger to main

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,30 @@ import {
   FastifyAdapter,
 } from '@nestjs/platform-fastify';
 import { AppModule } from '~/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(process.env.PORT ?? 3310);
+
+  const PORT = process.env.PORT ?? 3310;
+
+  const config = new DocumentBuilder()
+    .setTitle('Clickone shop backend API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .setDescription('Description')
+    .addServer(`http://localhost:${PORT}/api`, 'Development server')
+    .addServer('https://...', 'Production server')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT);
 }
+
 void bootstrap();
