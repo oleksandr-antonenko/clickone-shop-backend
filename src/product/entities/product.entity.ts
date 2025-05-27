86a6a4ebc0c45bbd35b1b9c97250af9b01052f92
vgodun <vgodunsoftware@gmail.com>
2025-05-27 22:19:47 +0300

    feauture: products

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,34 @@
-export class Product {}
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ProductFamily } from "./product-family.entity";
+// import { ProductOptionValue } from "./product-option-value.entity";
+// import { ProductSetting } from "./product-setting.entity";
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  stock: number;
+
+  @Column()
+  storage: number;
+
+  @Column()
+  chip: string;
+
+  @ManyToOne(()=>ProductFamily, family => family.products)
+  family: ProductFamily;
+  
+//   @OneToMany(()=>ProductOptionValue, option => option.product)
+//   options: ProductOptionValue[];
+
+//   @OneToMany(()=>ProductSetting, setting => setting.product)
+//   settings: ProductSetting[];
+}
