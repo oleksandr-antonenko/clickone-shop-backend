4d183e9765824101230ca376cda3922e665155e7
yevhenns <gifestum@gmail.com>
2025-06-10 17:27:40 +0300

    entity description

--- src/catalog/settings/entity/product-setting.entity.ts
+++ src/catalog/settings/entity/product-setting.entity.ts
@@ -1,17 +1,49 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-import { Product } from "../../product/entities/product.entity";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Product } from '../../product/entities/product.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
-@Entity('product_settings')
+@Entity('settings')
 export class ProductSetting {
+  @ApiProperty({ description: 'Unique identifier of the settings' })
   @PrimaryGeneratedColumn()
   id: number;
-  @Column()
-  key:string;
 
-  @Column()
-  value:string;
+  @ApiProperty({
+    description: 'Product setting name',
+    required: true,
+  })
+  @Column({ type: 'varchar', length: 255 })
+  key: string;
 
-  @ManyToOne(()=> Product, product => product.settings)
+  @ApiProperty({
+    description: 'Product setting value',
+    required: true,
+  })
+  @Column({ type: 'varchar', length: 255 })
+  value: string;
+
+  @ApiProperty({ description: 'Creation date', readOnly: true })
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @ApiProperty({ description: 'Last update date', readOnly: true })
+  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @ApiProperty({
+    description: 'Product relationship',
+    type: () => [Product],
+    required: false,
+  })
+  @ManyToOne(() => Product, (product) => product.settings, { nullable: true })
   @JoinColumn({ name: 'product_id' })
   product: Product;
 }
