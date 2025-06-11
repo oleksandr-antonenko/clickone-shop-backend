6739d24594fbf8bfcb7030299f435b0284dee85e
yevhenns <gifestum@gmail.com>
2025-06-11 15:23:32 +0300

    import Product

--- src/catalog/settings/settings.module.ts
+++ src/catalog/settings/settings.module.ts
@@ -3,9 +3,10 @@ import { SettingsService } from './service/settings.service';
 import { SettingsController } from './controller/settings.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductSetting } from './entity/product-setting.entity';
+import { Product } from '../product/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductSetting])],
+  imports: [TypeOrmModule.forFeature([ProductSetting, Product])],
   controllers: [SettingsController],
   providers: [SettingsService],
   exports: [SettingsService],
