ba33cbd1c01c91f4d7d0bc34c0690591a7a9fbeb
vgodun <vgodunsoftware@gmail.com>
2025-06-14 17:56:55 +0300

    fix: error message

--- src/catalog/category/service/category.service.ts
+++ src/catalog/category/service/category.service.ts
@@ -122,7 +122,9 @@ export class CategoryService {
   }
 
   async remove(id: number) {
-    try {
+
+    try 
+    {
       const removedCategory = await this.categoryRepository.findOne({
         where: { id },
       });
