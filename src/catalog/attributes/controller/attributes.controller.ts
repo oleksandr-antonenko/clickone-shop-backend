98a0175e48e433e26bf81be2fa612cf70e6c9237
Yevhenns <gifestum@gmail.com>
2025-06-15 15:48:13 +0300

    create type

--- src/catalog/attributes/controller/attributes.controller.ts
+++ src/catalog/attributes/controller/attributes.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AttributesService } from '../service/attributes.service';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { CreateAttributesTypeDto } from '../dto/create-attributes-type.dto';
 
 @Controller('attributes')
-export class AttributesController {}
+export class AttributesController {
+  constructor(private readonly attributesService: AttributesService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Create a new attributes type' })
+  @ApiResponse({
+    status: 201,
+    description: 'Attributes type created successfully',
+  })
+  @ApiBody({ type: CreateAttributesTypeDto })
+  async create(@Body() createAttributesTypeDto: CreateAttributesTypeDto) {
+    return this.attributesService.createType(createAttributesTypeDto);
+  }
+}
--- src/catalog/attributes/service/attributes.service.ts
+++ src/catalog/attributes/service/attributes.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { AttributeType } from '../entity/attributes-type.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateAttributesTypeDto } from '../dto/create-attributes-type.dto';
 
 @Injectable()
-export class AttributesService {}
+export class AttributesService {
+  constructor(
+    @InjectRepository(AttributeType)
+    private attributesTypeRepository: Repository<AttributeType>,
+  ) {}
+
+  async createType(createAttributesTypeDto: CreateAttributesTypeDto) {
+    const createAttributesType = this.attributesTypeRepository.create({
+      ...createAttributesTypeDto,
+    });
+
+    try {
+      return await this.attributesTypeRepository.save(createAttributesType);
+    } catch (error) {
+      console.error('CreateProductFamily error:', error);
+      throw new BadRequestException('Failed to create attributes type');
+    }
+  }
+}
