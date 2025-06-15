1a523892abd58720df4ebb5504bde1d35258a5a1
Yevhenns <gifestum@gmail.com>
2025-06-15 15:09:14 +0300

    type entity update

--- src/catalog/attributes/entity/attributes-type.entity.ts
+++ src/catalog/attributes/entity/attributes-type.entity.ts
@@ -1,17 +1,25 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { AttributeValue } from "./attributes-value.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { AttributeValue } from './attributes-value.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('attribute_types')
 export class AttributeType {
+  @ApiProperty({ description: 'Unique identifier of the attributes type' })
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @ApiProperty({ description: 'Name of the attributes type', required: true })
+  @Column({ type: 'varchar', length: 255 })
   name: string;
 
-  @Column()
+  @ApiProperty({ description: 'Slug of the attributes type', required: true })
+  @Column({ type: 'varchar', length: 255 })
   slug: string;
 
-  @OneToMany(()=>AttributeValue, value => value.type)
+  @ApiProperty({
+    description: 'AttributeValue relationship',
+    type: () => [AttributeValue],
+  })
+  @OneToMany(() => AttributeValue, (value) => value.type)
   values: AttributeValue[];
 }
