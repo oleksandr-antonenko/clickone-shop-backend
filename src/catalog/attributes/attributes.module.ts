3812bdf7dee67fb7fe99117ce24e6e6b7808bdc7
Yevhenns <gifestum@gmail.com>
2025-06-21 09:17:26 +0300

    import type orm

--- src/catalog/attributes/attributes.module.ts
+++ src/catalog/attributes/attributes.module.ts
@@ -6,8 +6,19 @@ import { AttributesTypeService } from './service/attributes-type.service';
 import { AttributesValueService } from './service/attributes-value.service';
 import { AttributesValueController } from './controller/attributes-value.controller';
 import { AttributesService } from './service/attributes.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductOptionValue } from './entity/attributes-option-value.entity';
+import { AttributeValue } from './entity/attributes-value.entity';
+import { AttributeType } from './entity/attributes-type.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      ProductOptionValue,
+      AttributeType,
+      AttributeValue,
+    ]),
+  ],
   providers: [AttributesService, AttributesTypeService, AttributesValueService],
   controllers: [
     AttributesController,
