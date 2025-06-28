845960dcca9102acdb163ad878124058f049784c
Yevhenns <gifestum@gmail.com>
2025-06-28 14:46:03 +0300

    added swagger info to controller

--- src/catalog/brand/controler/brand.controller.ts
+++ src/catalog/brand/controler/brand.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 import { CreateBrandDto } from '../dto/create-brand.dto';
 import { UpdateBrandDto } from '../dto/update-brand.dto';
@@ -17,26 +18,52 @@ export class BrandController {
   constructor(private readonly brandService: BrandService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new brand' })
+  @ApiResponse({
+    status: 201,
+    description: 'Brand created successfully',
+  })
+  @ApiBody({ type: CreateBrandDto })
   create(@Body() createBrandDto: CreateBrandDto) {
     return this.brandService.create(createBrandDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all brands' })
+  @ApiResponse({
+    status: 200,
+    description: 'Brands fetched successfully',
+  })
   findAll() {
     return this.brandService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a brand by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Brand fetched successfully',
+  })
   findOne(@Param('id') id: string) {
     return this.brandService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a brand by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Brand updated successfully',
+  })
   update(@Param('id') id: string, @Body() updateBrandDto: UpdateBrandDto) {
     return this.brandService.update(+id, updateBrandDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a brand by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Brand deleted successfully',
+  })
   remove(@Param('id') id: string) {
     return this.brandService.remove(+id);
   }
