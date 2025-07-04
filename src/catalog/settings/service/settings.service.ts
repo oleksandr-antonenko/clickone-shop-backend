66479a4a177e64e88cfccde83c9dab987fb59be6
Yevhen Ziuskin <103356318+Yevhenns@users.noreply.github.com>
2025-07-04 09:58:26 +0300

    Feature/settings (#11)
    * entity description
    
    * find all
    
    * get one by id
    
    * entity rename
    
    * added interfaces
    
    * added dto
    
    * delete setting
    
    * create setting
    
    * update
    
    * import Product
    
    * fix: add relations product
    
    * prettier
    
    * added logger
    
    * added decorators
    
    * family relation
    
    * removed double dots
    
    * added types
    
    * removed product id check
    
    ---------
    
    Co-authored-by: vgodun <vgodunsoftware@gmail.com>
    Co-authored-by: vgodun <146578980+vgodun@users.noreply.github.com>

--- src/catalog/settings/service/settings.service.ts
+++ src/catalog/settings/service/settings.service.ts
@@ -1,4 +1,170 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Injectable,
+  Logger,
+  NotFoundException,
+  Param,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductSetting } from '../entity/product-setting.entity';
+import { Repository } from 'typeorm';
+import { Product } from '~/catalog/product/entities/product.entity';
+import { CreateSettingDto } from '../dto/create-setting.dto';
+import { UpdateSettingDto } from '../dto/update-setting.dto';
+import { UpdateSetting } from '../interface/updateSetting.interface';
 
 @Injectable()
-export class SettingsService {}
+export class SettingsService {
+  constructor(
+    @InjectRepository(ProductSetting)
+    private settingsServiceRepository: Repository<ProductSetting>,
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>
+  ) {}
+  private readonly logger = new Logger(SettingsService.name);
+
+  private async findProductById(productId?: number): Promise<Product> {
+    try {
+      const product = await this.productRepository.findOne({
+        where: { id: productId },
+      });
+      if (!product) {
+        throw new NotFoundException(`Product with ID ${productId} not found`);
+      }
+
+      return product;
+    } catch (error) {
+      const err = error as Error;
+      this.logger.error(
+        `FindProductById error with ID ${productId}: ${err.message}`,
+        err.stack
+      );
+      throw new BadRequestException('Failed to find product');
+    }
+  }
+
+  async create(
+    @Body() createSettingDto: CreateSettingDto
+  ): Promise<ProductSetting> {
+    try {
+      const product = await this.findProductById(createSettingDto.productId);
+
+      const productSetting = this.settingsServiceRepository.create({
+        ...createSettingDto,
+        product: product ?? undefined,
+      });
+
+      return await this.settingsServiceRepository.save(productSetting);
+    } catch (error) {
+      const err = error as Error;
+      this.logger.error(
+        `CreateProductSetting error: ${err.message}`,
+        err.stack
+      );
+      throw new BadRequestException('Failed to create product setting');
+    }
+  }
+
+  async findAll(): Promise<ProductSetting[]> {
+    try {
+      return await this.settingsServiceRepository.find({
+        relations: ['product'],
+      });
+    } catch (error) {
+      const err = error as Error;
+      this.logger.error(
+        `FindAllProductSettings error: ${err.message}`,
+        err.stack
+      );
+      throw new BadRequestException('Failed to find product settings');
+    }
+  }
+
+  async findOne(@Param('id') id: number): Promise<ProductSetting> {
+    try {
+      const setting = await this.settingsServiceRepository.findOne({
+        where: {
+          id,
+        },
+        relations: ['product'],
+      });
+
+      if (!setting) {
+        throw new NotFoundException(`Setting with ID ${id} not found`);
+      }
+
+      return setting;
+    } catch (error) {
+      const err = error as Error;
+      this.logger.error(
+        `FindOneProductSetting error: ${err.message}`,
+        err.stack
+      );
+      throw new BadRequestException('Failed to find product setting');
+    }
+  }
+
+  async update(
+    @Param('id') id: number,
+    @Body() updateSettingDto: UpdateSettingDto
+  ): Promise<ProductSetting> {
+    try {
+      const setting = await this.settingsServiceRepository.findOne({
+        where: { id },
+      });
+
+      if (!setting)
+        throw new NotFoundException(`Product setting with ID ${id} not found`);
+
+      const product = await this.findProductById(updateSettingDto.productId);
+
+      const updateDto: UpdateSetting = {
+        key: updateSettingDto.key ?? setting.key,
+        value: updateSettingDto.value ?? setting.value,
+      };
+
+      const productSetting = this.settingsServiceRepository.create({
+        ...updateDto,
+        product: product ?? undefined,
+      });
+
+      const updated = this.settingsServiceRepository.merge(setting, {
+        ...productSetting,
+      });
+
+      return await this.settingsServiceRepository.save(updated);
+    } catch (error) {
+      const err = error as Error;
+      this.logger.error(
+        `UpdateProductSetting error: ${err.message}`,
+        err.stack
+      );
+      throw new BadRequestException('Failed to update product setting');
+    }
+  }
+
+  async remove(@Param('id') id: number): Promise<{
+    message: string;
+  }> {
+    try {
+      const setting = await this.settingsServiceRepository.findOne({
+        where: { id },
+      });
+
+      if (!setting)
+        throw new NotFoundException(`Product setting with ID ${id} not found`);
+
+      await this.settingsServiceRepository.remove(setting);
+
+      return { message: 'Product setting deleted successfully' };
+    } catch (error) {
+      const err = error as Error;
+      this.logger.error(
+        `RemoveProductSetting error: ${err.message}`,
+        err.stack
+      );
+      throw new BadRequestException('Failed to delete product setting');
+    }
+  }
+}
--- src/catalog/settings/settings.module.ts
+++ src/catalog/settings/settings.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { SettingsController } from './controller/settings.controller';
 import { ProductSetting } from './entity/product-setting.entity';
+import { Product } from '../product/entities/product.entity';
 import { SettingsService } from './service/settings.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductSetting])],
+  imports: [TypeOrmModule.forFeature([ProductSetting, Product])],
   controllers: [SettingsController],
   providers: [SettingsService],
+  exports: [SettingsService],
 })
 export class SettingsModule {}
